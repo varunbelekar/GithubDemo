f129629a4dcafbd7464dbc741c44a20e05586dc8
varun belekar <vbele@nets.eu>
2019-04-11 14:29:41 +0530

    Adding name

--- GithubOps/src/com/varun/GitHubDemo.java
+++ GithubOps/src/com/varun/GitHubDemo.java
@@ -4,7 +4,7 @@ public class GitHubDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String names[]= {"varun","aru","anu","ash"};
+		String names[]= {"varun","aru","anu","ash","abasdh"};
 		
 		for(int i=0;i<names.length;i++) {
 			System.out.println(names[i]);
