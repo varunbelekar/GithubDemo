2dae006e98b804a6ee7b8f4df55a0ed4f301f1ef
varun belekar <vbele@nets.eu>
2019-04-11 12:53:47 +0530

    Initial commit Dev1.

---
+++ GithubOps/src/com/varun/GitHubDemo.java
@@ -0,0 +1,14 @@
+package com.varun;
+
+public class GitHubDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String names[]= {"varun","aru","anu","ash"};
+		
+		for(int i=0;i<names.length;i++) {
+			System.out.println(names[i]);
+		}
+	}
+
+}
